42d24266cc4038cfd5ada86b5121d738ada809dc
Ivan <Kushkovova81@gmail.com>
2023-11-06 18:01:58 +0300

    dd
    d

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ import requests
 from functools import partial
 
 from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QMovie
+from PyQt5.QtWidgets import QPushButton, QHBoxLayout, QWidget, QVBoxLayout, QLabel
+from pymongo import MongoClient, DESCENDING
 
 from MainWindow import Ui_MainDialog
+
+
+def getAllEffects():
+    mongo = MongoClient("mongodb+srv://sovyshka:ujdyfdnhzgjxre@cluster0.rpsi7sd.mongodb.net/")
+    database = mongo["leddb"]
+    collection = database["visualeffects"]
+
+    document = collection.find("effect")
+
+    return document
 
 
 class MainWindowDialog(QtWidgets.QDialog):
@@ -15,29 +28,50 @@ class MainWindowDialog(QtWidgets.QDialog):
         self.ui = Ui_MainDialog()
         self.ui.setupUi(self)
 
-        self.getAllEffects()
+        self.printEffects()
 
-    def getAllEffects(self):
-        url = "http://localhost:8080/api/getalleffects"
-        response = requests.get(url)
+    def printEffects(self):
+        mongo = MongoClient("mongodb+srv://sovyshka:ujdyfdnhzgjxre@cluster0.rpsi7sd.mongodb.net/")
+        database = mongo["leddb"]
+        collection = database["visualeffects"]
 
-        if response.status_code == 200:
-            data = response.json()
-            row = 0
-            col = 0
+        cursor = collection.find().sort("popularity", DESCENDING)
 
-            for item in data:
-                button = QPushButton(item["effectName"])
-                self.ui.gridLayout.addWidget(button, row, col)
-                col += 1
+        row = 0
+        col = 0
 
-                if col == 5:
-                    col = 0
-                    row += 1
+        for item in cursor:
+            layout = QVBoxLayout()
 
-                button.clicked.connect(partial(self.startEffect, item))
-        else:
-            print("Ошибка при выполнении запроса. Статус код:", response.status_code)
+            button = QPushButton(item.get('name'))
+            button.setFixedSize(200, 30)
+            button.setStyleSheet("background: #2B2D2F;\n"
+                                 "color: #71DFBE;\n"
+                                 "border-radius: 4px;\n"
+                                 "font: bold 1.25rem/1 poppins;\n"
+                                 )
+
+            image_label = QLabel()
+            pixmap = QMovie(f"gif/{item.get('image')}.gif")
+            image_label.setMovie(pixmap)
+            pixmap.start()
+
+            layout.addWidget(image_label, alignment=Qt.AlignHCenter)
+            layout.addWidget(button, alignment=Qt.AlignHCenter)
+
+            widget = QWidget()
+            widget.setLayout(layout)
+            widget.setStyleSheet("QPushButton{border: 1px solid #71DFBE;}")
+            layout.setSizeConstraint(QVBoxLayout.SetFixedSize)
+
+            self.ui.gridLayout.addWidget(widget, row, col)
+            col += 1
+
+            if col == 5:
+                col = 0
+                row += 1
+
+            button.clicked.connect(partial(self.startEffect, item))
 
     def startEffect(self, item):
         import socket
@@ -48,18 +82,31 @@ class MainWindowDialog(QtWidgets.QDialog):
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_socket.connect((host, port))
 
-            data = json.dumps({"effect": item["effect"]})
+            data = json.dumps({"effect": item.get('effect')})
 
             client_socket.send(data.encode())
-            self.addPopularity(item)
             client_socket.close()
+            self.addPopularity(item)
+
         except Exception as e:
             print(e)
 
     def addPopularity(self, item):
-        effectName = item["effect"]
-        url = f"http://localhost:8080/api/increasepopularity/{effectName}"
-        requests.get(url)
+        mongo = MongoClient("mongodb+srv://sovyshka:ujdyfdnhzgjxre@cluster0.rpsi7sd.mongodb.net/")
+        database = mongo["leddb"]
+        collection = database["visualeffects"]
+
+        query = {"effect": item.get('effect')}
+        document = collection.find_one(query)
+
+        if document:
+            new_popularity = document.get("popularity", 0) + 1
+            collection.update_one(query,
+                                  {"$set":
+                                      {
+                                          "popularity": new_popularity
+                                      }
+                                  })
 
 
 if __name__ == "__main__":
